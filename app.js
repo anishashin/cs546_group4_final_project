2327966ec7ccaba7e0f97f837609fd6d44483afc
Kushal Patel <kushal1223@gmail.com>
2021-11-26 10:32:40 -0500

    Created handlebars files and outline for login/signup. Still need to connect them to routes and work on outlining other handlebar files

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const app = express();
-const configRoutes = require('./routes');
+const static = express.static(__dirname + '/public');
+const session = require('express-session');
 
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+
+app.use('/public', static);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(
+    session({
+      name: 'AuthCookie',
+      secret: "Secret Code",
+      saveUninitialized: true,
+      resave: false
+    })
+);
+  
+app.use('/private', (req, res, next) => {
+    console.log(req.session.id);
+    if (!req.session.user) {
+      return res.redirect('/');
+    } else {
+      next();
+    }
+});
+
 configRoutes(app);
 
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
 });
