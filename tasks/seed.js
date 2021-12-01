7464de5ba522f64427e6b9ac4cefeaa34d52020d
anishashin <anishashin@gmail.com>
2021-12-01 15:13:55 -0500

    added more foods to seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -14,20 +14,28 @@ const main = async () => {
     const lasya = await users.create('Lasya', 'Josyula', 'lasyajosyula', 'password3');
     const peixin = await users.create('Peixin', 'Dai', 'peixindai', 'password4');
 
-    const pizza = await foods.create('Cheese Pizza', 1, 'medium slice', 'medium slices', 213, 7.8, 27, 9.1);
-    const burger = await foods.create('Hamburger Patty', 3, 'oz', 'oz', 197, 12, 0, 21);
-    const rice = await foods.create('White Rice', 1, 'cup', 'cups', 205, 0.4, 45, 4.3);
-    const pasta = await foods.create('Rigatoni', 1, 'cup', 'cups', 160, 0.9, 31, 5.9);
+    const cheese_pizza = await foods.create('Cheese Pizza', 1, 'medium slice', 'medium slices', 213, 7.8, 27, 9.1);
+    
+    const rigatoni = await foods.create('Rigatoni', 1, 'cup', 'cups', 160, 0.9, 31, 5.9);
 
-    const meal1 = await savedPlates.create(anisha._id, "Anisha's Meal", [pasta._id], [2]);
-    const meal2 = await savedPlates.create(kushal._id, "Kushal's Meal", [pizza._id, pasta._id], [3, 2]);
-    const meal3 = await savedPlates.create(lasya._id, "Lasya's Meal", [burger._id, rice._id], [1, 2]);
-    const meal4 = await savedPlates.create(peixin._id, "Peixin's Meal", [burger._id, pizza._id], [1, 2]);
+    const hamburger_patty = await foods.create('Hamburger Patty', 3, 'oz', 'oz', 197, 12, 0, 21);
+    const hot_dog = await foods.create('Hot Dog', 1, 'frankfurter', 'frankfurters', 155, 14, 1.3, 5.6);
+    const grilled_chicken = await foods.create('Grilled Chicken', 3, 'oz', 'oz', 126, 2.9, 0, 25);
+    const chicken_nuggets = await foods.create('Chicken Nuggets', 5, 'piece', 'pieces', 245, 16.5, 12, 12.5);
+    const tater_tots = await foods.create('Tater Tots', 10, 'tot', 'tots', 161, 7.6, 23, 1.8);
+    
+    const white_rice = await foods.create('White Rice', 1, 'cup', 'cups', 205, 0.4, 45, 4.3);
+    const brown_rice = await foods.create('Brown Rice', 1, 'cup', 'cups', 218, 1.6, 46, 4.6);
 
-    const comment1 = await comments.create(pasta._id, anisha._id, 'The pasta is delicious!');
-    const comment2 = await comments.create(pizza._id, kushal._id, 'My favorite midnight snack!');
-    const comment3 = await comments.create(burger._id, lasya._id, 'The burger could use a little more flavor...');
-    const comment4 = await comments.create(pizza._id, peixin._id, 'Highly recommend the pizza!');
+    const meal1 = await savedPlates.create(anisha._id, "Anisha's Meal", [rigatoni._id], [2]);
+    const meal2 = await savedPlates.create(kushal._id, "Kushal's Meal", [cheese_pizza._id, rigatoni._id], [3, 2]);
+    const meal3 = await savedPlates.create(lasya._id, "Lasya's Meal", [hamburger_patty._id, white_rice._id], [1, 2]);
+    const meal4 = await savedPlates.create(peixin._id, "Peixin's Meal", [hamburger_patty._id, cheese_pizza._id], [1, 2]);
+
+    const comment1 = await comments.create(rigatoni._id, anisha._id, 'The pasta is delicious!');
+    const comment2 = await comments.create(cheese_pizza._id, kushal._id, 'My favorite midnight snack!');
+    const comment3 = await comments.create(hamburger_patty._id, lasya._id, 'The burger could use a little more flavor...');
+    const comment4 = await comments.create(cheese_pizza._id, peixin._id, 'Highly recommend the pizza!');
 
     console.log('Done seeding database');
     await db.serverConfig.close();
