abc30b3f30d5dbe1acb03ff8771638f32e94f84c
anishashin <anishashin@gmail.com>
2021-12-01 20:26:37 -0500

    added functionality to search bar

---
+++ public/js/search.js
@@ -0,0 +1,15 @@
+(function($) {
+    $('#search').on('keyup change', function(event) {
+        event.preventDefault();
+        const search_term = $('#search').val().toLowerCase();
+        const foods = $('.card-title');
+        for(let i = 0; i < foods.length; i++) {
+            if(foods[i].innerHTML.toLowerCase().includes(search_term)) {
+                $(foods[i]).closest('.col')[0].style.display = 'initial';
+            }
+            else {
+                $(foods[i]).closest('.col')[0].style.display = 'none';        
+            }
+        }
+    });
+})(window.jQuery);
