0f9da309910cfaf4d7dea952a9279aa2933643e5
anishashin <60356251+anishashin@users.noreply.github.com>
2021-12-03 12:05:03 -0500

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,10 @@
-(function ($) {
-    $('#search').on('keyup change', function (event) {
+(function($) {
+    $('#search').on('keyup change', function(event) {
         event.preventDefault();
         const search_term = $('#search').val().toLowerCase();
         const foods = $('.card-title');
-        for (let i = 0; i < foods.length; i++) {
-            if (foods[i].textContent.toLowerCase().includes(search_term)) {
+        for(let i = 0; i < foods.length; i++) {
+            if(foods[i].textContent.toLowerCase().includes(search_term)) {
                 $(foods[i]).closest('.col')[0].style.display = 'initial';
             }
             else {
