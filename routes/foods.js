c88ff49c2f98d995cc1ecd64fe29f1a0f9c37ca8
anishashin <anishashin@gmail.com>
2021-12-06 00:12:54 -0500

    edit food bug fixes after testing

--- routes/foods.js
+++ routes/foods.js
@@ -28,8 +28,8 @@ router.get('/edit/:id', async (req, res) => {
         return;
     }
     try {
-        const food = await foodData.get(req.params.id);
-        res.status(200).render('edit_food', {title: 'Edit Food', food: food});
+        const foodInfo = await foodData.get(req.params.id);
+        res.status(200).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo});
     } catch (e) {
         res.status(404).json({error: 'Food not found.'});
     }
@@ -126,44 +126,45 @@ router.put('/:id', async (req, res) => {
     }
     let foodInfo = req.body;
     if(!foodInfo) {
-        res.status(400).json({error: 'You must provide data to edit a food.'});
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'You must provide data to edit a food.'});
         return;
     }
-    foodInfo.servingSizeNumber = parseFloat(foodInfo.servingSizeNumber);
-    foodInfo.calories = parseFloat(foodInfo.calories);
-    foodInfo.fat = parseFloat(foodInfo.fat);
-    foodInfo.carbs = parseFloat(foodInfo.carbs);
-    foodInfo.protein = parseFloat(foodInfo.protein);
+    foodInfo._id = req.params.id;
+    foodInfo.parsedServingSizeNumber = parseFloat(foodInfo.servingSizeNumber);
+    foodInfo.parsedCalories = parseFloat(foodInfo.calories);
+    foodInfo.parsedFat = parseFloat(foodInfo.fat);
+    foodInfo.parsedCarbs = parseFloat(foodInfo.carbs);
+    foodInfo.parsedProtein = parseFloat(foodInfo.protein);
     if(!foodInfo.name || typeof foodInfo.name !== 'string' || foodInfo.name.trim() === '') {
-        res.status(400).json({error: 'Name must be a non-empty string containing more than just spaces.'});
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Invalid food name.'});
         return;
     }
-    if(!foodInfo.servingSizeNumber || typeof foodInfo.servingSizeNumber !== 'number' || foodInfo.servingSizeNumber <= 0) {
-        res.status(400).json({error: 'Serving size number must be a number greater than 0.'});
+    if(!foodInfo.parsedServingSizeNumber || typeof foodInfo.parsedServingSizeNumber !== 'number' || foodInfo.parsedServingSizeNumber <= 0) {
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Serving size (number) must be a number greater than 0.'});
         return;
     }
     if(!foodInfo.servingSizeUnitSingular || typeof foodInfo.servingSizeUnitSingular !== 'string' || foodInfo.servingSizeUnitSingular.trim() === '') {
-        res.status(400).json({error: 'Serving size unit (singular) must be a non-empty string containing more than just spaces.'});
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Invalid serving size unit (singular).'});
         return;
     }
     if(!foodInfo.servingSizeUnitPlural || typeof foodInfo.servingSizeUnitPlural !== 'string' || foodInfo.servingSizeUnitPlural.trim() === '') {
-        res.status(400).json({error: 'Serving size unit (plural) must be a non-empty string containing more than just spaces.'});
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Invalid serving size unit (plural).'});
         return;
     }
-    if(typeof foodInfo.calories !== 'number' || isNaN(foodInfo.calories) || foodInfo.calories < 0) {
-        res.status(400).json({error: 'Calories must be a number greater than or equal to 0.'});
+    if(typeof foodInfo.parsedCalories !== 'number' || isNaN(foodInfo.parsedCalories) || foodInfo.parsedCalories < 0) {
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Calories must be a number greater than or equal to 0.'});
         return;
     }
-    if(typeof foodInfo.fat !== 'number' || isNaN(foodInfo.fat) || foodInfo.fat < 0) {
-        res.status(400).json({error: 'Fat must be a number greater than or equal to 0.'});
+    if(typeof foodInfo.parsedFat !== 'number' || isNaN(foodInfo.parsedFat) || foodInfo.parsedFat < 0) {
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Fat must be a number greater than or equal to 0.'});
         return;
     }
-    if(typeof foodInfo.carbs !== 'number' || isNaN(foodInfo.carbs) || foodInfo.carbs < 0) {
-        res.status(400).json({error: 'Carbs must be a number greater than or equal to 0.'});
+    if(typeof foodInfo.parsedCarbs !== 'number' || isNaN(foodInfo.parsedCarbs) || foodInfo.parsedCarbs < 0) {
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Carbohydrates must be a number greater than or equal to 0.'});
         return;
     }
-    if(typeof foodInfo.protein !== 'number' || isNaN(foodInfo.protein) || foodInfo.protein < 0) {
-        res.status(400).json({error: 'Protein must be a number greater than or equal to 0.'});
+    if(typeof foodInfo.parsedProtein !== 'number' || isNaN(foodInfo.parsedProtein) || foodInfo.parsedProtein < 0) {
+        res.status(400).render('edit_food', {title: 'Edit Food', foodInfo: foodInfo, error: 'Protein must be a number greater than or equal to 0.'});
         return;
     }
     try {
@@ -176,13 +177,13 @@ router.put('/:id', async (req, res) => {
         const updatedFood = await foodData.update(
             req.params.id,
             foodInfo.name,
-            foodInfo.servingSizeNumber,
+            foodInfo.parsedServingSizeNumber,
             foodInfo.servingSizeUnitSingular,
             foodInfo.servingSizeUnitPlural,
-            foodInfo.calories,
-            foodInfo.fat,
-            foodInfo.carbs,
-            foodInfo.protein
+            foodInfo.parsedCalories,
+            foodInfo.parsedFat,
+            foodInfo.parsedCarbs,
+            foodInfo.parsedProtein
         );
         res.redirect('/foods/' + req.params.id);
     } catch (e) {
